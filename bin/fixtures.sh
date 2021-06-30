e2c0c4496331bc91588698027fdd95779cff45fd
jmsche <contact@jmsche.fr>
2021-06-30 11:34:26 +0200

    Sync recipes

--- bin/fixtures.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bin/console doctrine:database:drop --force
-bin/console doctrine:database:create
-bin/console doctrine:schema:update --force
-bin/console hautelook:fixtures:load --no-interaction --no-bundles
