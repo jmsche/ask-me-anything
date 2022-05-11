0fea9a6cccca23d2daf4e431d3e49e4a5d807cde
jmsche <contact@jmsche.fr>
2022-05-11 09:31:20 +0200

    Upgrade Symfony to 6.0.8 & other vendors & require PHP 8.1.5+

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,12 +9,12 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 final class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    public function __construct(ManagerRegistry $registry, private UserPasswordHasherInterface $passwordHasher)
+    public function __construct(ManagerRegistry $registry, private readonly UserPasswordHasherInterface $passwordHasher)
     {
         parent::__construct($registry, User::class);
     }
@@ -22,7 +22,7 @@ final class UserRepository extends ServiceEntityRepository implements PasswordUp
     /**
      * Used to upgrade (rehash) the user's password automatically over time.
      */
-    public function upgradePassword(UserInterface $user, string $newHashedPassword): void
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
     {
         if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
